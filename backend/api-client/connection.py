843fd7d7e1eac972680919f022420d6eb7d86c76
Arthur Santana dos Santos <arthurssantos@ibm.com>
2026-01-13 09:12:55 -0300

    Load environment variables for database connection and create main.py file

--- backend/api-client/connection.py
+++ backend/api-client/connection.py
@@ -1,11 +1,15 @@
 import mysql.connector
 from mysql.connector import errorcode, pooling, Error
+from dotenv import load_dotenv
+import os
 
-DB_PASSWORD = "1701076288"
-DB_USER = "admin"
-DB_HOST = "databaseproject.cc1kujauuftf.us-east-1.rds.amazonaws.com"
+load_dotenv(dotenv_path="C:/Users/ArthurSantanadosSant/Documents/banco-javer-crud/backend/models/core/.env")
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_USER = os.getenv("DB_USER")
+DB_HOST = os.getenv("DB_HOST")
 DB_PORT = 3306
-DB_NAME = "javer"
+DB_NAME = os.getenv("DB_NAME")
 
 try:
     connection_pool = pooling.MySQLConnectionPool(
@@ -15,7 +19,8 @@ try:
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME,
-        port=DB_PORT
+        port=DB_PORT,
+        use_pure=True
     )
     print("Conexão com o banco de dados estabelecida com sucesso.")
 
